aa56af8e508edb55de6b1b6f2702fc1292d2e01c
the-abadie <davidevan3605@gmail.com>
2025-02-15 20:41:25 -0500

    Update Install

--- install.sh
+++ install.sh
@@ -1,34 +1,32 @@
 #!/bin/bash
 
-read -p "Installing Python Prerequisites. Are you in a virtual environment with Python 3.9? [Y/N]" pythonVer
+read -p "Installing Python Prerequisites. Are you in a virtual environment with Python 3.9? [Y/N]\n" pythonVer
 
 case $pythonVer in
-	[Yy] ) echo Proceeding with pip installs; break;;
-	[Nn] ) echo Please use a venv with Python 3.9, exiting...; exit 1; break;;
-	*    ) echo Please use a venv with Python 3.9, exiting...; exit 1; break;;
+	[Yy] ) echo Proceeding with pip installs...;;
+	[Nn] ) echo Please use a venv with Python 3.9, exiting...; exit 1;;
+	*    ) echo Please use a venv with Python 3.9, exiting...; exit 1;;
 esac
 
-echo Proceeding with pip installs.
-pip install numpy tensorflow tensorflow-addons protobuf transformers regex pubchempy sympy spacy torch chemdataextractor psutil pymongo torch tqdm scipy gensim
+pip -v install numpy tensorflow==2.7.0 tensorflow-addons==0.17.1 protobuf==3.19.6 transformers>=4.11.3 regex pubchempy sympy spacy torch chemdataextractor psutil pymongo torch tqdm scipy gensim
 
 cd MaterialParser
-echo Attemping MaterialParser Install
+echo Attemping MaterialParser install
 pip install . && echo MaterialParser Installed Successfully
 
 cd ../MatEntityRecognition
-echo Attempting MaterialRecognizer Install
+echo Attempting MaterialRecognizer install
 pip install . && echo MaterialRecognizer Installed Successfully
 
 cd ../ParagraphClassification
-echo Attempting ParagraphClassifier Install
+echo Attempting ParagraphClassifier install
 pip install . && python -m synthesis_classifier.model download && echo ParagraphClassifier Installed Sucessfully
 
 cd ../OperationsExtraction
-echo Attemping OperationsExtraction Install
+echo Attemping OperationsExtraction install
 pip install . && echo OperationsExtraction Installed Sucessfully
 
 cd ../ReactionCompleter
-echo Attemping ReactionCompleter Install
+echo Attemping ReactionCompleter install
 pip install . && echo ReactionCompleter Installed Sucessfully
 
-echo Installed.
