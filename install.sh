a31f320973d7597e45751b14cfc538528f72e992
the-abadie <davidevan3605@gmail.com>
2025-02-15 20:42:59 -0500

    Update Install

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ case $pythonVer in
 	*    ) echo Please use a venv with Python 3.9, exiting...; exit 1;;
 esac
 
-pip -v install numpy tensorflow==2.7.0 tensorflow-addons==0.17.1 protobuf==3.19.6 transformers>=4.11.3 regex pubchempy sympy spacy torch chemdataextractor psutil pymongo torch tqdm scipy gensim
+pip -v install numpy tensorflow==2.7.0 tensorflow-addons==0.17.1 protobuf==3.19.6 transformers>4.11.3 regex pubchempy sympy spacy torch chemdataextractor psutil pymongo torch tqdm scipy gensim
 
 cd MaterialParser
 echo Attemping MaterialParser install
