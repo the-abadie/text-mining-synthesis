dddd57b85643cd43c7a9d7201d1ebb157da23d3a
the-abadie <davidevan3605@gmail.com>
2025-03-06 14:06:49 -0500

    Updated install

--- install.sh
+++ install.sh
@@ -9,21 +9,20 @@ case $pythonVer in
 	*    ) echo Please use a venv with Python 3.9, exiting...; exit 1;;
 esac
 
-#pip install -v numpy "tensorflow-addons==0.17.1" tensorflow "protobuf==3.19.6" "transformers==4.11.3" regex pubchempy sympy spacy torch chemdataextractor psutil pymongo torch tqdm scipy gensim
+pip install -v numpy "tensorflow-addons==0.17.1" tensorflow "protobuf==3.19.6" "transformers==4.11.3" regex pubchempy sympy spacy torch chemdataextractor psutil pymongo torch tqdm scipy gensim
 
 cd MaterialParser
 echo Attemping MaterialParser install
-pip install -e . && echo MaterialParser Installed Successfully
+pip install -e .
 
 cd ../MatEntityRecognition
 echo Attempting MaterialRecognizer install
-pip install -e . && echo MaterialRecognizer Installed Successfully
+pip install -e .
 
 cd ../ParagraphClassification
 echo Attempting ParagraphClassifier install
 pip install -e .
 python -m synthesis_classifier.model download 
-echo ParagraphClassifier Installed Sucessfully
 
 cde data download
 
